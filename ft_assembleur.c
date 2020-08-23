cf5e9d47c1cd62066f7d394ee59f7e60f676264d
Marcaday <noah.gr.addino@gmail.com>
2020-08-23 17:54:33 +0200

    up

--- ft_assembleur.c
+++ ft_assembleur.c
@@ -1,54 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-char assembleur(char **str, char *ass1, char *ass2)
+char assembleur(char **ptr_str, char *ass1, char *ass2)
 {
     int i = 0;
     int x = 0;
+    char *str = malloc(sizeof(char) * (strlen(ass1)  + strlen(ass2) + 1));
 
-    while(str[i])
+    while(ass1[x])
     {
+        str[x] = ass1[x];
+        x++;
+    }
+    i = x;
+    x = 0;
+    while(ass2[x])
+    {
+        str[i] = ass2[x];
+        x++;
         i++;
     }
-    if(ass1[x] != '\0')
-    {
-        while(ass1[x])
-        {
-            str[i][x] = ass1[x];
+    str[i] = '\0';
+    *ptr_str = str;
 
-            
-            x++;
-        }
-        
-        x = 0;
-    }
-    if(ass2[x] != '\0')
-    {
-        while(ass2[x])
-        {
-            str[i][x] = ass2[x];
-
-            
-            x++;
-        }
-    }
 
 
 }
 int main()
 {
     int x = 0;
-    
+
     char ass1[] = "je suis une tchoin";
     char ass2[] = "tu ne ressemble a r";
 
-    char str[15][10000];
+    char **str;
 
     assembleur(str, ass1, ass2);
-
-    while(str[x])
-    {
-        printf("%s", str[x]);
-        
-        x++;
-    }
+    printf("%s", *str);
 }
